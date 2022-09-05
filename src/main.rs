c63b340f4b1cc325ed4cf6dbf2bf1de3e9d6871a
Sam H. Smith <sam.henning.smith@protonmail.com>
2022-09-05 23:21:27 +0200

    more unicode fixes

--- src/main.rs
+++ src/main.rs
@@ -56,12 +56,12 @@ fn main() {
         }
         let mut cursor_line: isize = 0;
         let mut cursor_column: isize = 0;
-        let mut window_padding : usize = 0; //Gets set dynamically
+        let mut window_padding: usize = 0; //Gets set dynamically
         let mut show_line_nums = true;
         let mut window_start = 0;
         let mut running = true;
 
-        let mut current_indentation : usize = 0;
+        let mut current_indentation: usize = 0;
 
         let mut insert_mode = false;
         let mut file_mode = FileMode::Edit;
@@ -101,7 +101,7 @@ fn main() {
                         Ok(0) => {
                             buffer.pop();
                             break;
-                            },
+                        }
                         Ok(_) => (),
                         _ => panic!(),
                     }
@@ -183,9 +183,20 @@ fn main() {
                                 if insert_mode {
                                     match c.unwrap() {
                                         Key::Char(c) if c == '\n' => {
-                                            let mut newstr = buffer[cursor_line as usize]
-                                                .split_off(cursor_column as usize);
-                                            for i in 0..current_indentation { newstr.insert(0, ' '); }
+                                            let mut tmp = 0;
+                                            {
+                                                let mut chars =
+                                                    buffer[cursor_line as usize].chars();
+                                                for i in 0..cursor_column {
+                                                    tmp += chars.next().unwrap().len_utf8();
+                                                }
+                                            }
+
+                                            let mut newstr =
+                                                buffer[cursor_line as usize].split_off(tmp);
+                                            for i in 0..current_indentation {
+                                                newstr.insert(0, ' ');
+                                            }
                                             buffer.insert(cursor_line as usize + 1, newstr);
                                             cursor_column = current_indentation as isize;
                                             cursor_line += 1;
@@ -193,29 +204,55 @@ fn main() {
                                         Key::Char(c) if c == '\t' => {
                                             let pad_count = 4 - (cursor_column % 4);
                                             for x in 0..pad_count {
-                                                let index = if cursor_column < buffer[cursor_line as usize].chars().count() as isize
+                                                let index = if cursor_column
+                                                    < buffer[cursor_line as usize].chars().count()
+                                                        as isize
                                                 {
-                                                    buffer[cursor_line as usize].char_indices()
-                                                    .nth(cursor_column as usize).unwrap().0
-                                                }
-                                                else { buffer[cursor_line as usize].len() };
-                                                buffer[cursor_line as usize]
-                                                    .insert(index, ' ');
+                                                    buffer[cursor_line as usize]
+                                                        .char_indices()
+                                                        .nth(cursor_column as usize)
+                                                        .unwrap()
+                                                        .0
+                                                } else {
+                                                    buffer[cursor_line as usize].len()
+                                                };
+                                                buffer[cursor_line as usize].insert(index, ' ');
                                                 cursor_column += 1;
                                             }
                                         }
                                         Key::Ctrl(c) if c == 't' => {
-                                            buffer[cursor_line as usize].insert(cursor_column as usize, '\t');
+                                            buffer[cursor_line as usize]
+                                                .insert(cursor_column as usize, '\t');
                                             cursor_column += 1;
                                         }
                                         Key::Char(c) if c == '}' => {
-                                            if cursor_column >= 4 &&
-                                               buffer[cursor_line as usize].chars().nth(cursor_column as usize - 1).unwrap() == ' ' &&
-                                               buffer[cursor_line as usize].chars().nth(cursor_column as usize - 2).unwrap() == ' ' &&
-                                               buffer[cursor_line as usize].chars().nth(cursor_column as usize - 3).unwrap() == ' ' &&
-                                               buffer[cursor_line as usize].chars().nth(cursor_column as usize - 4).unwrap() == ' '
+                                            if cursor_column >= 4
+                                                && buffer[cursor_line as usize]
+                                                    .chars()
+                                                    .nth(cursor_column as usize - 1)
+                                                    .unwrap()
+                                                    == ' '
+                                                && buffer[cursor_line as usize]
+                                                    .chars()
+                                                    .nth(cursor_column as usize - 2)
+                                                    .unwrap()
+                                                    == ' '
+                                                && buffer[cursor_line as usize]
+                                                    .chars()
+                                                    .nth(cursor_column as usize - 3)
+                                                    .unwrap()
+                                                    == ' '
+                                                && buffer[cursor_line as usize]
+                                                    .chars()
+                                                    .nth(cursor_column as usize - 4)
+                                                    .unwrap()
+                                                    == ' '
                                             {
-                                                buffer[cursor_line as usize].replace_range(((cursor_column as usize) - 4)..(cursor_column as usize), "");
+                                                buffer[cursor_line as usize].replace_range(
+                                                    ((cursor_column as usize) - 4)
+                                                        ..(cursor_column as usize),
+                                                    "",
+                                                );
                                                 cursor_column -= 4;
                                             }
                                             buffer[cursor_line as usize]
@@ -223,26 +260,42 @@ fn main() {
                                             cursor_column += 1;
                                         }
                                         Key::Char(c) => {
-                                            let index = if cursor_column < buffer[cursor_line as usize].chars().count() as isize
+                                            let index = if cursor_column
+                                                < buffer[cursor_line as usize].chars().count()
+                                                    as isize
                                             {
-                                                buffer[cursor_line as usize].char_indices()
-                                                .nth(cursor_column as usize).unwrap().0
-                                            }
-                                            else { buffer[cursor_line as usize].len() };
-                                            buffer[cursor_line as usize]
-                                                .insert(index, c);
+                                                buffer[cursor_line as usize]
+                                                    .char_indices()
+                                                    .nth(cursor_column as usize)
+                                                    .unwrap()
+                                                    .0
+                                            } else {
+                                                buffer[cursor_line as usize].len()
+                                            };
+                                            buffer[cursor_line as usize].insert(index, c);
                                             cursor_column += 1;
                                         }
                                         Key::Backspace => {
                                             if cursor_column > 0 {
                                                 cursor_column -= 1;
+                                                let mut tmp = 0;
+                                                let mut tmp_size = 0;
+                                                {
+                                                    let mut chars =
+                                                        buffer[cursor_line as usize].chars();
+                                                    for i in 0..cursor_column {
+                                                        tmp += chars.next().unwrap().len_utf8();
+                                                    }
+                                                    tmp_size = chars.next().unwrap().len_utf8();
+                                                }
                                                 buffer[cursor_line as usize]
-                                                    .remove(cursor_column as usize);
+                                                    .replace_range(tmp..(tmp + tmp_size), "");
                                             } else if cursor_line > 0 {
                                                 let st = buffer.remove(cursor_line as usize);
-                                                let length = buffer[cursor_line as usize - 1].len();
-                                                buffer[cursor_line as usize - 1]
-                                                    .insert_str(length, &st);
+                                                let length = buffer[cursor_line as usize - 1]
+                                                    .chars()
+                                                    .count();
+                                                buffer[cursor_line as usize - 1].push_str(&st);
                                                 cursor_line -= 1;
                                                 cursor_column = length as isize;
                                             }
@@ -253,9 +306,7 @@ fn main() {
                                 } else {
                                     match c.unwrap() {
                                         // Exit.
-                                        Key::Char(c) if c == 'q' => {
-                                            running = false
-                                        }
+                                        Key::Char(c) if c == 'q' => running = false,
                                         Key::Char(c) if c == 'l' => {
                                             show_line_nums = !show_line_nums;
                                         }
@@ -317,10 +368,20 @@ fn main() {
                                         Key::Char(c) if c == 'd' => {
                                             if (cursor_line as usize) < buffer.len()
                                                 && (cursor_column as usize)
-                                                    < buffer[cursor_line as usize].len()
+                                                    < buffer[cursor_line as usize].chars().count()
                                             {
+                                                let mut tmp = 0;
+                                                let mut tmp_size = 0;
+                                                {
+                                                    let mut chars =
+                                                        buffer[cursor_line as usize].chars();
+                                                    for i in 0..cursor_column {
+                                                        tmp += chars.next().unwrap().len_utf8();
+                                                    }
+                                                    tmp_size = chars.next().unwrap().len_utf8();
+                                                }
                                                 buffer[cursor_line as usize]
-                                                    .remove(cursor_column as usize);
+                                                    .replace_range(tmp..(tmp + tmp_size), "");
                                             } else if (cursor_line as usize + 1) < buffer.len() {
                                                 let st = buffer.remove(cursor_line as usize + 1);
                                                 buffer[cursor_line as usize]
@@ -330,8 +391,18 @@ fn main() {
                                         Key::Backspace => {
                                             if cursor_column > 0 {
                                                 cursor_column -= 1;
+                                                let mut tmp = 0;
+                                                let mut tmp_size = 0;
+                                                {
+                                                    let mut chars =
+                                                        buffer[cursor_line as usize].chars();
+                                                    for i in 0..cursor_column {
+                                                        tmp += chars.next().unwrap().len_utf8();
+                                                    }
+                                                    tmp_size = chars.next().unwrap().len_utf8();
+                                                }
                                                 buffer[cursor_line as usize]
-                                                    .remove(cursor_column as usize);
+                                                    .replace_range(tmp..(tmp + tmp_size), "");
                                             } else {
                                                 if cursor_line as usize > buffer.len() {
                                                     cursor_line -= 1;
@@ -380,7 +451,9 @@ fn main() {
                                         }
                                         Key::Char(c) if c == 'o' => {
                                             if (cursor_line as usize) < buffer.len() {
-                                                cursor_column = buffer[cursor_line as usize].chars().count() as isize;
+                                                cursor_column =
+                                                    buffer[cursor_line as usize].chars().count()
+                                                        as isize;
                                             } else {
                                                 cursor_column = current_indentation as isize;
                                             }
@@ -426,11 +499,12 @@ fn main() {
                 window_padding = 0;
             }
 
-            if (cursor_line as usize) < buffer.len()
-            {
+            if (cursor_line as usize) < buffer.len() {
                 current_indentation = 0;
                 for c in buffer[cursor_line as usize].chars() {
-                    if c != ' ' { break; }
+                    if c != ' ' {
+                        break;
+                    }
                     current_indentation += 1;
                 }
             }
@@ -484,21 +558,24 @@ fn main() {
                     );
 
                     let mut remainder_are_spaces_index = line.chars().count();
-                    while remainder_are_spaces_index > 0 &&
-                          line.chars().nth(remainder_are_spaces_index - 1).unwrap() == ' ' &&
-                          (remainder_are_spaces_index > cursor_column as usize || (index + window_start) != cursor_line as usize)
+                    while remainder_are_spaces_index > 0
+                        && line.chars().nth(remainder_are_spaces_index - 1).unwrap() == ' '
+                        && (remainder_are_spaces_index > cursor_column as usize
+                            || (index + window_start) != cursor_line as usize)
                     {
                         remainder_are_spaces_index -= 1;
                     }
                     for (i, c) in line.chars().enumerate() {
-                        if i >= remainder_are_spaces_index { break; }
+                        if i >= remainder_are_spaces_index {
+                            break;
+                        }
                         render_buffer.push(c);
                     }
-                    render_buffer.push_str(termion::style::Underline{}.as_ref());
+                    render_buffer.push_str(termion::style::Underline {}.as_ref());
                     for i in remainder_are_spaces_index..line.chars().count() {
                         render_buffer.push_str("%");
                     }
-                    render_buffer.push_str(termion::style::NoUnderline{}.as_ref());
+                    render_buffer.push_str(termion::style::NoUnderline {}.as_ref());
 
                     render_buffer.push_str(termion::color::Reset {}.fg_str());
                     render_buffer.push_str(termion::color::Reset {}.bg_str());
@@ -565,7 +642,11 @@ fn main() {
                 if (cursor_line as usize) < buffer.len() {
                     for i in 0..cursor_column {
                         if (i as usize) < buffer[cursor_line as usize].chars().count() {
-                            if buffer[cursor_line as usize].chars().nth(i as usize).unwrap() == '\t'
+                            if buffer[cursor_line as usize]
+                                .chars()
+                                .nth(i as usize)
+                                .unwrap()
+                                == '\t'
                             {
                                 tab_count += 1;
                             }
@@ -594,5 +675,3 @@ fn main() {
     }
     println!("Thank you for using dte!");
 }
-
-
